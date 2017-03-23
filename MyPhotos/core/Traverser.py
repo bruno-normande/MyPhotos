244cbc7ab7a71f5fab0e2deb769b92aedd582db9
Bruno Normande <normandelins@gmail.com>
2017-03-23 17:37:07 -0300

    Adding list of "not supported" files

--- MyPhotos/core/Traverser.py
+++ MyPhotos/core/Traverser.py
@@ -19,6 +19,8 @@ class Traverser(object):
         total_files =  int(os.popen("find {} -not -path '*/\.*' -type f | wc -l".format(self._source) ).read())
         count = 1
 
+        not_sup = []
+
         for dirName, subdirList, fileList in os.walk(self._source):
 
             print "{} de {} - {}%".format(count, total_files, count*100/total_files)
@@ -26,11 +28,17 @@ class Traverser(object):
             for file in fileList:
                 file_path = os.path.join(dirName, file)
                 type = magic.from_file(file_path).split()[0]
-                if type in ['JPEG', 'PNG']:
+                if type == 'JPEG':
                     print 'imagem:', file
                     im = MyImage(file_path, type)
+                else:
+                    not_sup.append(file_path)
 
                     # print im
 
                 count += 1
 
+        print " ------------- FILES NOT SUPORTED ------------- "
+        for i in not_sup:
+            print i
+        print " ------------- xxxxxxxxxxxxxxxxxx ------------- "
